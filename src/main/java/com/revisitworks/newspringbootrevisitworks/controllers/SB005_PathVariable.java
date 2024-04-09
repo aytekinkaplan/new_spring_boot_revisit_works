a04eac565d35fa38258339ed452921d32b6df15f
AYTEKİN KAPLAN <112782378+aytekinkaplan@users.noreply.github.com>
2024-04-09 22:43:49 +0300

    New Commit

---
+++ src/main/java/com/revisitworks/newspringbootrevisitworks/controllers/SB005_PathVariable.java
@@ -0,0 +1,10 @@
+package com.revisitworks.newspringbootrevisitworks.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "/path/variable")
+public class SB005_PathVariable {
+}
+
